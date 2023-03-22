36ba6004ddbf9f126071e805f26eea4ac10d8e2c
Mikhail Zhvakin <mikhail.zhvakin@auriga.com>
2023-03-22 22:04:00 +0300

    Fix mongo start in KOS qemu: change compiler -march option to armv8-a
    Trying to run mongod in KOS qemu causes the program to crash with an
    "Unhandled Generic Exception" message and a stack trace.
    
    Investigating the crash log it was discovered that the problem is
    connected with fetch_add() function (atomic add). Investigating the
    disassembler listing it was found that the problematic instruction is
    LDADDAL. Support for this instruction  was introduced in ARMv8.1.
    
    For *aarch64* target architecture, the SConstruct script sets the -march
    compiler option to armv8.2-a. KOS qemu does not support armv8.2-a. The
    most supported is armv8-a.
    
    Signed-off-by: Mikhail Zhvakin <mikhail.zhvakin@auriga.com>

--- build-kos.sh
+++ build-kos.sh
@@ -14,6 +14,12 @@ SYSROOT_PATH=${SDK_PATH}/sysroot-${TARGET_ARCH}-${TARGET_OS}
 CC=${TARGET_ARCH}-${TARGET_OS}-gcc
 CXX=${TARGET_ARCH}-${TARGET_OS}-g++
 
+# For aarch64 target architecture, the SConstruct script sets the -march
+# compiler option to armv8.2-a. KOS qemu does not support armv8.2-a. This
+# causes some CPU instructions to fail.
+# The most supported is armv8-a. Back to armv8-a.
+MARCH=armv8-a
+
 # TODO:
 # 1) Switch the build to dynamic linking when supported by the KOS CE SDK;
 # 2) Enable support for HTTP client & Free Monitoring when libcurl is available
@@ -29,7 +35,7 @@ python3 buildscripts/scons.py \
     --use-system-boost \
     CC="${SDK_PATH}/toolchain/bin/${CC}" \
     CXX="${SDK_PATH}/toolchain/bin/${CXX}" \
-    CCFLAGS="--sysroot=${SYSROOT_PATH}" \
+    CCFLAGS="--sysroot=${SYSROOT_PATH} -march=${MARCH}" \
     CXXFLAGS="--sysroot=${SYSROOT_PATH}" \
     TARGET_OS="${TARGET_OS}" \
     install-mongod -j${NPROC}
