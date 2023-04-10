905f62faf98a07f3935402a87013b8190da158e7
Mikhail Zhvakin <mikhail.zhvakin@auriga.com>
2023-04-10 14:08:43 +0300

    Remove '--network host' option from docker scripts
    Signed-off-by: Mikhail Zhvakin <mikhail.zhvakin@auriga.com>

--- kos/docker/create-and-run-container.sh
+++ kos/docker/create-and-run-container.sh
@@ -2,11 +2,10 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 
-# Note: Instead of a "host" network, you can create your own docker network and
+# Note: Instead of default "bridge" network, you can create your own docker network and
 # share it between containers.
 docker run -it --name ${USER}-kos-ce-mongo \
     -v ${SCRIPT_DIR}/../../:/root/mongo \
-    --network host \
     --hostname kos-mongod-server \
     "$@" \
     debian-kos-ce-mongo:1.1.1.13
