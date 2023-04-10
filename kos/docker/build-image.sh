9704272d8cdb3f47427d5b6f42ee5a2a43fdf417
Mikhail Zhvakin <mikhail.zhvakin@auriga.com>
2023-04-10 14:49:48 +0300

    Fix docker image building script
    Signed-off-by: Mikhail Zhvakin <mikhail.zhvakin@auriga.com>

--- kos/docker/build-image.sh
+++ kos/docker/build-image.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
 
-docker build -t debian-kos-ce-mongo:1.1.1.13 -f Dockerfile ${SCRIPT_DIR}/../../etc/ \
+docker build -t debian-kos-ce-mongo:1.1.1.13 -f ${SCRIPT_DIR}/Dockerfile \
+    ${SCRIPT_DIR}/../../etc/ \
     && echo "Docker image build status: SUCCESS" \
     || echo "Docker image build status: FAILED"
